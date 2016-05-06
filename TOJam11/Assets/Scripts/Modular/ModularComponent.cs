363d486f3009696e2e8f802039776f6f952bcf9d
netgrind <cale.bradbury@gmail.com>
2016-05-06 19:13:58 -0400

    Car testing

---
+++ TOJam11/Assets/Scripts/Modular/ModularComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum ModularType
+{
+    Chasis,
+    Wheel,
+    Body,
+    Gun
+}
+
+public class ModularComponent : MonoBehaviour {
+
+    public ModularType type;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	} 
+}
