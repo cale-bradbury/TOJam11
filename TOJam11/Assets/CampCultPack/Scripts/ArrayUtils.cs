3ed5909e821e48d037f3b62693e79d133991dabd
netgrind <cale.bradbury@gmail.com>
2016-05-06 22:25:56 -0400

    Array utils are now typeless

--- TOJam11/Assets/CampCultPack/Scripts/ArrayUtils.cs
+++ TOJam11/Assets/CampCultPack/Scripts/ArrayUtils.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ArrayUtils 
 {
-	public static int[,,] Create3D(int fill,int x, int y, int z){
-		int[,,] a = new int[x,y,z];
+	public static T[,,] Create3D<T>(T fill,int x, int y, int z){
+		T[,,] a = new T[x,y,z];
 		for(int i = 0; i<x;i++){
 			for(int j = 0; j<y;j++){
 				for(int k = 0; k<z;k++){
@@ -14,21 +14,39 @@ public class ArrayUtils
 		}
 		return a;
 	}
-	public static int[,] Create2D(int fill,int x, int y){
-		int[,] a = new int[x,y];
-		for(int i = 0; i<x;i++){
-			for(int j = 0; j<y;j++){
-				a[i,j] = fill;
-			}
-		}
-		return a;
+    public static T[,] Create2D<T>(T fill, int x, int y)
+    {
+        T[,] a = new T[x, y];
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                a[i, j] = fill;
+            }
+        }
+        return a; 
+    }
+    
+    public static T[,] Create2D<T>(System.Func<int,int,T> func, int x, int y)
+    {
+        T[,] a = new T[x, y];
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                a[i, j] = func(i,j);
+            }
+        }
+        return a;
+    }
+
+    public static T[, ,] Clone3D<T>(T[, ,] a)
+    {
+		return a.Clone() as T[,,];
 	}
 
-	public static int[,,] Clone3D(int[,,] a){
-		return a.Clone() as int[,,];
-	}
-
-	public static int[,,] Merge3D(int[,,] parent, int[,,] child, int x, int y, int z){
+    public static T[, ,] Merge3D<T>(T[, ,] parent, T[, ,] child, int x, int y, int z)
+    {
 		for(int i = 0; i<child.GetLength(0);i++){
 			for(int j = 0; j<child.GetLength(1);j++){
 				for(int k = 0; k<child.GetLength(2);k++){
@@ -41,21 +59,23 @@ public class ArrayUtils
 		}
 		return parent;
 	}
-	
-	public static int[,,] SwapValue3D(int[,,] a, int oldValue, int newValue){
+
+    public static T[, ,] SwapValue3D<T>(T[, ,] a, T oldValue, T newValue)
+    {
 		for(int i = 0; i<a.GetLength(0);i++){
 			for(int j = 0; j<a.GetLength(1);j++){
 				for(int k = 0; k<a.GetLength(2);k++){
-					if(a[i,j,k]==oldValue) a[i,j,k]=newValue;
+					if(a[i,j,k].Equals(oldValue)) a[i,j,k]=newValue;
 				}
 			}
 		}
 		return a;
 	}
 
-	public static int[,,] Flood3D(int[,,] a, int x, int y, int z, int oldValue, int newValue){
+    public static T[, ,] Flood3D<T>(T[, ,] a, int x, int y, int z, T oldValue, T newValue)
+    {
 		if(x>-1 && x<a.GetLength(0) && y>-1 && y<a.GetLength(1) && z>-1 && z<a.GetLength(2)){
-			if(a[x,y,z] == oldValue){
+			if(a[x,y,z].Equals(oldValue)){
 				a[x,y,z] = newValue;
 				Flood3D(a,x-1,y,z,oldValue,newValue);
 				Flood3D(a,x+1,y,z,oldValue,newValue);
@@ -68,11 +88,12 @@ public class ArrayUtils
 		return a;
 	}
 
-	public static bool hasValue3D(int[,,] a, int value) {
+    public static bool hasValue3D<T>(T[, ,] a, T value)
+    {
 		for (int i = 0; i < a.GetLength(0); i++) {
 			for(int j = 0; j<a.GetLength(1);j++){
 				for(int k = 0; k<a.GetLength(2);k++){
-					if(a[i,j,k]==value)return true;
+					if(a[i,j,k].Equals(value))return true;
 				}
 			}
 		}
