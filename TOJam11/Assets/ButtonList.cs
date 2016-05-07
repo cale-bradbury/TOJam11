ab011ecae04ae21d897f60ebe424948378b735eb
netgrind <cale.bradbury@gmail.com>
2016-05-07 01:42:09 -0400

    buttons highlight nice

--- TOJam11/Assets/ButtonList.cs
+++ TOJam11/Assets/ButtonList.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class ButtonList : MonoBehaviour {
@@ -6,22 +7,75 @@ public class ButtonList : MonoBehaviour {
     public GameObject buttonPrefab;
     public RectTransform buttonParent;
     public int count;
-    List<GameObject> buttons;
-    
+    List<Button> buttons;
+    Button current;
+    int _selected = 0;
+    int selected
+    {
+        get { return _selected; }
+        set
+        {
+            value = Mathf.Max(value, 0);
+            value = Mathf.Min(value, buttons.Count - 1);
+            _selected = value;
+            OnSelect();
+        }
+    }
+
+    public Color defaultColor = Color.white;
+    public Color selectedColor = Color.red;
+    ScrollRect scroll;
+    float scrollTarget = 0;
+    public float scrollSmoothing = .1f;
 
 	// Use this for initialization
 	void Start () {
-        buttons = new List<GameObject>();
+        scroll = GetComponent<ScrollRect>();
+        buttons = new List<Button>();
         for (int i = 0; i < count; i++)
         {
             GameObject g = Instantiate<GameObject>(buttonPrefab);
             g.transform.SetParent( buttonParent);
-            buttons.Add(g);
+            Button b = g.GetComponent<Button>();
+            b.onClick.AddListener(() => { OnClick(b); });
+            buttons.Add(b);
         }
 	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            selected++;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            selected--;
+
+        if (Input.mouseScrollDelta.y < 0)
+            selected++; 
+        else if (Input.mouseScrollDelta.y > 0)
+            selected--;
+
+        scroll.verticalNormalizedPosition = Mathf.Lerp(scroll.verticalNormalizedPosition, scrollTarget, scrollSmoothing);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void OnClick (Button b) {
+        selected = buttons.IndexOf(b);
 	}
+
+    void OnSelect()
+    {
+        Debug.Log(selected);
+        ColorBlock block;
+        if (current!=null)
+        {
+            block = current.colors;
+            block.normalColor = block.highlightedColor = defaultColor;
+            current.colors = block;
+        }
+        current = buttons[selected];
+        block = current.colors;
+        block.normalColor = block.highlightedColor = selectedColor;
+        current.colors = block;
+        scrollTarget = 1 - (float)current.transform.GetSiblingIndex() / ((float)scroll.content.transform.childCount-1);
+    }
 }
