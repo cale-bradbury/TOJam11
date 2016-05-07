d85b41d5690d75381dc98429c5078cd74cb62f9f
Erik Krieg <erik.c.krieg@gmail.com>
2016-05-07 16:43:11 -0400

    Prop spawner now moves and stores props.

--- TOJam11/Assets/Scripts/Environment/PropSpawner.cs
+++ TOJam11/Assets/Scripts/Environment/PropSpawner.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PropSpawner : MonoBehaviour {
     public GameObject[] smallPropPrefabs;
@@ -16,17 +16,43 @@ public class PropSpawner : MonoBehaviour {
     [HideInInspector]
     public float[] largePropSpawn;
     private float lastSpawnAttempt = 0f;
+    private List<Renderer> props = new List<Renderer>();
 
     void Start()
     {
         currentSpawnChance = minSpawnChance;
     }
 
-    void Update() {
-        if ( lastSpawnAttempt + spawnRate < Time.time && ShouldSpawnProp())
+    void Update() {        
+        AttemptSpawnProp();
+        UpdateProps();
+    }
+
+    void AttemptSpawnProp()
+    {
+        if (lastSpawnAttempt + spawnRate < Time.time && ShouldSpawnProp())
         {
             lastSpawnAttempt = Time.time;
             SpawnProp();
+        }
+    }
+
+    void UpdateProps()
+    {
+        for(var i = 0; i < props.Count; i++)
+        {
+            Renderer prop = props[i];
+            Vector3 tempPos = prop.transform.position;
+            float z = Time.deltaTime * scrollSpeed * 11.13f;
+            tempPos.z -= z;
+            prop.transform.position = tempPos;
+
+            if (props[i].transform.position.z < transform.position.z && !props[i].isVisible)
+            {                
+                props.RemoveAt(i);
+                Destroy(prop.gameObject);
+                i--;
+            }
         }
     }
 
@@ -48,7 +74,7 @@ public class PropSpawner : MonoBehaviour {
             prop = Instantiate(ArrayUtils.getRandom<GameObject>(largePropPrefabs), tempPos, Quaternion.identity) as GameObject;
         }
         prop.transform.parent = transform;
-        prop.GetComponent<PropScroller>().scrollSpeed = scrollSpeed * 11.13f;
+        props.Add(prop.GetComponent<Renderer>());
     }
 
     bool ShouldSpawnProp() {
