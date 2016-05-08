684533c9148bd0c89166a75fba2333af94b2c346
Erik Krieg <erik.c.krieg@gmail.com>
2016-05-07 22:59:58 -0400

    Location nodes can now trigger state change to battles.

--- TOJam11/Assets/States/Game/Overworld.cs
+++ TOJam11/Assets/States/Game/Overworld.cs
@@ -4,18 +4,36 @@ using System;
 
 public class Overworld : GameState
 {
+    // store a ref active node
+
+    GameStateMachine gsm;
+    MapController activeLevel;
+
     public override void Awake()
     {
-
+        gsm = GetComponent<GameStateMachine>();
+        // When there is more than one level, we will need to request specific levels.
+        GameObject g = Instantiate<GameObject>(Resources.Load<GameObject>("Levels/LevelOneMapController"));
+        activeLevel = g.GetComponent<MapController>();
+        activeLevel.overworld = this;
     }
 
     public override void Update()
     {
-
+        
     }
+
     public override void OnDestroy()
     {
-
+        Destroy(activeLevel.gameObject);
     }
 
+    public void StartEncounter(LocationNode node, bool isRandomEncounter) {
+        // TODO:
+        // Once battles are developed, we will need to do some configuring based on data in the node
+        // to ensure the correct battle state is entered.
+        //TODO:
+        // cache a reference to this node's name or ID so that if the player comes back to this state they are at the correct node.
+        gsm.SwitchState<Battle>();
+    }
 }
