0a39da93184c316275ab86dedc106f18b26dd7d7
netgrind <cale.bradbury@gmail.com>
2016-05-07 22:52:19 -0400

    Builder Button

---
+++ TOJam11/Assets/Scripts/UI/PrefabDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PrefabDisplay : MonoBehaviour {
+
+    public GameObject prefab;
+    public string name;
+    public RawImage img;
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        img.texture = FindObjectOfType<CaptureCamera>().CapturePrefab(prefab,128,128);
+        text.text = name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
