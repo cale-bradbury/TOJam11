82e1a00a8bc21f9f71cd0928e1c2fbd3cd2142a6
Erik Krieg <erik.c.krieg@gmail.com>
2016-05-08 12:20:26 -0400

    Partially created stun action and debuff.

--- TOJam11/Assets/States/Game/Battle.cs
+++ TOJam11/Assets/States/Game/Battle.cs
@@ -4,9 +4,14 @@ using System;
 
 public class Battle : GameState
 {
+    GameStateMachine gsm;
+    BattleManager bm;
+
     public override void Awake()
     {
+        gsm = GetComponent<GameStateMachine>();
 
+        // Need to add a battle manager. Also need to know which specific battle to add...
     }
 
     public override void OnDestroy()
