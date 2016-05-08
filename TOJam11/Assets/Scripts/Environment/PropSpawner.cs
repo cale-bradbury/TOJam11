84795b80936eb36ddacfb14578c60a12830540c0
Erik Krieg <erik.c.krieg@gmail.com>
2016-05-07 20:42:13 -0400

    Prop spawner uses local positions.

--- TOJam11/Assets/Scripts/Environment/PropSpawner.cs
+++ TOJam11/Assets/Scripts/Environment/PropSpawner.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class PropSpawner : MonoBehaviour {
+public class PropSpawner : MonoBehaviour
+{
     public GameObject[] smallPropPrefabs;
     public GameObject[] largePropPrefabs;
     public float spawnRate = 50f;               // how often this will attempt to spawn props
@@ -23,7 +24,8 @@ public class PropSpawner : MonoBehaviour {
         currentSpawnChance = minSpawnChance;
     }
 
-    void Update() {        
+    void Update()
+    {
         AttemptSpawnProp();
         UpdateProps();
     }
@@ -39,16 +41,16 @@ public class PropSpawner : MonoBehaviour {
 
     void UpdateProps()
     {
-        for(var i = 0; i < props.Count; i++)
+        for (var i = 0; i < props.Count; i++)
         {
             Renderer prop = props[i];
-            Vector3 tempPos = prop.transform.position;
+            Vector3 tempPos = prop.transform.localPosition;
             float z = Time.deltaTime * scrollSpeed * 11.13f;
             tempPos.z -= z;
-            prop.transform.position = tempPos;
+            prop.transform.localPosition = tempPos;
 
-            if (props[i].transform.position.z < transform.position.z && !props[i].isVisible)
-            {                
+            if (props[i].transform.localPosition.z < 0 && !props[i].isVisible)
+            {
                 props.RemoveAt(i);
                 Destroy(prop.gameObject);
                 i--;
@@ -61,30 +63,33 @@ public class PropSpawner : MonoBehaviour {
         float xPos;
         bool small = Random.value < spawnSmallPropChance;
         GameObject prop;
-        Vector3 tempPos = transform.position;        
-        tempPos.z = 20f;    // TODO: this is just random right now. This value should be be related to screen size or something.
+        Vector3 tempPos = new Vector3(0f, 0f, 20f);
         if (small)
         {
             xPos = Random.Range(smallPropSpawn[0], smallPropSpawn[1]);
             tempPos.x = Random.value > 0.5 ? xPos : -xPos;
-            prop = Instantiate(ArrayUtils.getRandom<GameObject>(smallPropPrefabs), tempPos, Quaternion.identity) as GameObject;          
-        } else {
+            prop = Instantiate<GameObject>(ArrayUtils.getRandom<GameObject>(smallPropPrefabs));
+        }
+        else {
             xPos = Random.Range(largePropSpawn[0], largePropSpawn[1]);
             tempPos.x = Random.value > 0.5 ? xPos : -xPos;
-            prop = Instantiate(ArrayUtils.getRandom<GameObject>(largePropPrefabs), tempPos, Quaternion.identity) as GameObject;
+            prop = Instantiate<GameObject>(ArrayUtils.getRandom<GameObject>(largePropPrefabs));
         }
         prop.transform.parent = transform;
+        prop.transform.localPosition = tempPos;
         props.Add(prop.GetComponent<Renderer>());
     }
 
-    bool ShouldSpawnProp() {
+    bool ShouldSpawnProp()
+    {
         float chance = Random.value;
         bool spawn = chance < currentSpawnChance;
         currentSpawnChance += spawnChanceIncrement;
         if (spawn)
         {
             ResetSpawnChance();
-        } else
+        }
+        else
         {
             IncreaseSpawnChance();
         }
